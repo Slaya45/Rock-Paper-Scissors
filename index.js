f11d3ca7650eb94dbc1fdbe41ee2bd24a9fab34f
Slaya45 <caydenfrye@gmail.com>
2023-02-22 18:05:38 -0700

    Made an HTML boilerplate, added 2 <p> for the computer and the player added a little style for the player anc computer to make it have a better intactive feel

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+let computer = "00"
+let player = 0
+let computerEL = document.getElementById("computerChoice-el")
+let playerEL = document.getElementById("playerChoice-el")
+
+
+
+
+
+
+
+function compChoice() {
+  computerEL.textContent = "Computer" + " " + computer
+  
+
+}
+
+function startGame() {
+  let playerChoice =  "rock"
+  let computerChoice = compChoice()
+}
+console.log (compChoice)

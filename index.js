a1c4d47959c0945e99cabe572af7d0af1e5ef90d
Slaya45 <caydenfrye@gmail.com>
2023-03-08 21:39:31 -0700

    Connected all HTML elements to JS, created an random number choice for the computer. Then made a function for each button to give them identifiers and didplay the choice onto the screen when clicked

--- index.js
+++ index.js
@@ -1,14 +1,92 @@
 
+let playerScoreBoardEL = document.getElementById("playerScoreBoard-el")
+let computerScoreBoardEl = document.getElementById("computerScoreBoard-el")
+let computerChoiceEl = document.getElementById("computerChoice-el")
+let playerChoiceEl = document.getElementById("playerChoice-el")
+let resultEl = document.getElementById("result-el")
+const possibleChoices = document.querySelector ("button-el")
+let message = " "
+let playerMessage = " "
+let playerNumber = []
+let randomComputerChoices = []
 
 
-const computerChoice = document.getElementById("computerChoice")
-const playerChoice = document.getElementById("playerChoice")
-const result = document.getElementById("result")
-const possibleChoices = document.querySelector ("button")
+function computerChoice(){
+    let randomComputerChoices =  Math.floor( Math.random() * 3) + 1
+       if (randomComputerChoices > 2) {
+        computerChoiceEl.textContent = "Computer:" + " " + "Rock"
+        randomComputerChoices = "Rock"
+    } else if (randomComputerChoices === 2){
+        computerChoiceEl.textContent = "Computer:" + " " + "Scissors"
+        randomComputerChoices = "Scissors"
+    } else {
+        computerChoiceEl.textContent = "Computer:" + " " + "Paper"
+        randomComputerChoices = "Paper"
+    }
+    result()
+console.log (message)
+}
 
-let computer = 0
-let player = 0
+function buttonPaper(){
+    playerNumber =  "Rock"
+    playerMessage = "Player:" + " " + "Rock"
+    playerChoiceEl.textContent = playerMessage
 
+    console.log(playerMessage)
+}
+function buttonRock(){
+    playerNumber = "Scissors"
+    playerMessage = "Player:" + " " + "Scissors"
+    playerChoiceEl.textContent = playerMessage
+    
+    console.log(playerMessage)
+}
+function buttonScissors(){
+    playerNumber = "Paper"
+    playerMessage = "Player:" + " " + "Paper"
+    playerChoiceEl.textContent = playerMessage
+    
+    console.log(playerMessage)
+}
 
+function result(){
+    
+    if (randomComputerChoices === playerNumber){
+        resultEl.textContent = "Result:" + " " + "Tie!"
+    }else if(playerNumber == 'rock'){
+        if(computer == 'paper'){
+            result.textContent = 'Computer Won';
+            computerScore++;
+            computerScoreBoard.textContent = computerScore;
+
+        }else{
+            result.textContent = 'Player Won'
+            playerScore++;
+            playerScoreBoard.textContent = playerScore;
+        }
+    }
+    else if(player == 'scissors'){
+        if(computer == 'rock'){
+            result.textContent = 'Computer Won';
+            computerScore++;
+            computerScoreBoard.textContent = computerScore;
+        }else{
+            result.textContent = 'Player Won';
+            playerScore++;
+            playerScoreBoard.textContent = playerScore;
+        }
+    }
+    else if(player == 'paper'){
+        if(computer == 'scissors'){
+            result.textContent = 'Computer Won';
+            computerScore++;
+            computerScoreBoard.textContent = computerScore;
+        }else{
+            result.textContent = 'Player Won';
+            playerScore++;
+            playerScoreBoard.textContent = playerScore;
+        }
+    }
+}
 
 

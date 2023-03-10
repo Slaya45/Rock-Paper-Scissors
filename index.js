a95d3501a4ad34a8800f2817268267b9c2daa983
Slaya45 <caydenfrye@gmail.com>
2023-03-09 20:02:15 -0700

    Messing with the result function to make it work correctly currently the player wins everytime. Trying to debugg

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 
-let playerScoreBoardEL = document.getElementById("playerScoreBoard-el")
+let playerScoreBoardEl = document.getElementById("playerScoreBoard-el")
 let computerScoreBoardEl = document.getElementById("computerScoreBoard-el")
 let computerChoiceEl = document.getElementById("computerChoice-el")
 let playerChoiceEl = document.getElementById("playerChoice-el")
 let resultEl = document.getElementById("result-el")
-const possibleChoices = document.querySelector ("button-el")
-let message = " "
-let playerMessage = " "
+
+
 let playerChoice = " "
 let randomComputerChoices = " " 
-let playerScore = "Player Score" +  " " + 0
-let computerScore = "Computer Score" +  " " + 0
+let playerScore = 0
+let computerScore = 0
 
+function buttonPaper(){
+    playerChoice =  "Paper"
+    playerChoiceEl.textContent = "Player:" + " " + "Paper"
+    
+}
+function buttonRock(){
+    playerChoice = "Rock"
+    playerChoiceEl.textContent = "Player:" + " " + "Rock"
+    
+}
+function buttonScissors(){
+    playerChoice = "Scissors"
+    playerChoiceEl.textContent = "Player:" + " " + "Scissors"
+    
+}
 function computerChoice(){
     let randomComputerChoices =  Math.floor( Math.random() * 3) + 1
        if (randomComputerChoices > 2) {
@@ -24,73 +38,52 @@ function computerChoice(){
         computerChoiceEl.textContent = "Computer:" + " " + "Paper"
         randomComputerChoices = "Paper"
     }
-        result()
-        console.log (message)
-        console.log(randomComputerChoices)
-       
+    result()
+        
 }
 
-function buttonPaper(){
-    playerChoice =  "Rock"
-    playerMessage = "Player:" + " " + "Rock"
-    playerChoiceEl.textContent = playerMessage
 
-    console.log(playerMessage)
-    
-}
-function buttonRock(){
-    playerChoice = "Scissors"
-    playerMessage = "Player:" + " " + "Scissors"
-    playerChoiceEl.textContent = playerMessage
-    
-    console.log(playerMessage)
-}
-function buttonScissors(){
-    playerChoice = "Paper"
-    playerMessage = "Player:" + " " + "Paper"
-    playerChoiceEl.textContent = playerMessage
-    
-    console.log(playerMessage)
-}
 
 function result(){
     
-    if (randomComputerChoices === playerChoice){
-        resultEl.textContent = "Result:" + " " + "Tie!"
-    }else if(playerChoice == "rock"){
-        if(randomComputerChoices == "paper"){
-            resultEl.textContent = "Computer Won";
+    if (randomComputerChoices == playerChoice){
+        resultEl.textContent = "Tie!"
+    } else if (playerChoice == "Rock"){
+        if(randomComputerChoices == "Paper"){
+            resultEl.textContent = "Computer 1Won";
             computerScore++;
             computerScoreBoardEl.textContent = computerScore;
 
         }else{
-            resultEl.textContent = "Player Won"
+            resultEl.textContent == "Player 1Won"
             playerScore++;
-            playerScoreBoardEl.textContent = playerScore;
+            playerScoreBoardEl.textContent = "Your Score: " + playerScore;
         }
     }
-    else if(playerChoice == "scissors"){
-        if(randomComputerChoices == 'rock'){
-            resultEl.textContent = "Computer Won";
+    else if(playerChoice == "Scissors"){
+        if(randomComputerChoices == "Rock"){
+            resultEl.textContent = "Computer 2Won";
             computerScore++;
             computerScoreBoardEl.textContent = computerScore;
         }else{
-            resultEl.textContent = "Player Won";
+            resultEl.textContent = "Player 2Won";
             playerScore++;
-            playerScoreBoardEl.textContent = playerScore;
+            playerScoreBoardEl.textContent = "Your Score: " + playerScore;
         }
     }
-    else if(playerChoice == "paper"){
-        if(randomComputerChoices == "scissors"){
-            resultEl.textContent = "Computer Won";
+    else if(playerChoice == "Paper"){
+        if(randomComputerChoices == "Scissors"){
+            resultEl.textContent = "Computer 3Won";
             computerScore++;
             computerScoreBoardEl.textContent = computerScore;
         }else{
-            resultEl.textContent = "Player Won";
+            resultEl.textContent = "Player 3Won";
             playerScore++;
-            playerScoreBoardEl.textContent = playerScore;
+            playerScoreBoardEl.textContent = "Your Score: " + playerScore;
         }
     }
 }
 
 
+console.log(randomComputerChoices)
+console.log(playerChoice)
